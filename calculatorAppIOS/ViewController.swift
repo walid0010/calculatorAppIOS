d2dad48ec0843ef162eee3640a2a49649363b598
walid0010 <walidchekir98@gmail.com>
2021-07-20 07:54:38 +0100

    Initial Commit

---
+++ calculatorAppIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculatorAppIOS
+//
+//  Created by chekir walid on 20/7/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
